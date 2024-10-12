29487d175ff8b7a71bfed49375af629c57ac2e49
Christos <166133019+ChristosCDev@users.noreply.github.com>
2024-10-12 17:33:04 +0200

    .

---
+++ src/main/java/de/Christos/EarthLib.java
@@ -0,0 +1,16 @@
+package de.Christos;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EarthLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
